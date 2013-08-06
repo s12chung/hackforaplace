3edc988bbb8a9434b38d8b1ae08ca81b638fb226
s12chung <steve.chung7@gmail.com>
2013-08-06 10:53:19 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HackForAPlace::Application.config.secret_token = 'bba540c9e52660ab7248a49c212bcf0fef3e2cbac610461646753c51bf671c972442854ad59141213840ff6d27088295331ad7ad853cb23fb0a598d7424d6a9f'
