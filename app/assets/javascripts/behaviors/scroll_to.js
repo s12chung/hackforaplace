cd91c9a3b0aa4a475e0152d279d7cf085a040624
s12chung <steve.chung7@gmail.com>
2013-08-08 00:22:43 -0400

    scroll update nav

--- app/assets/javascripts/behaviors/scroll_to.js
+++ app/assets/javascripts/behaviors/scroll_to.js
@@ -1,8 +1,6 @@
 $(function() {
     $('body').on('click', data_behavior('scroll_to'), function(e) {
         e.preventDefault();
-        var href = $(this).attr('href');
-        var target = href === '#top' ? 0 : href;
-        $.scrollTo(target, 500, { offset: -80 });
+        $.scrollTo($(this).attr('href'), 500, { offset: -80 });
     });
 });
