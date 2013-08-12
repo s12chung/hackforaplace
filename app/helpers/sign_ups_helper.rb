71ef0d6ac24929a5a63870b02b6c4a96564c1c88
s12chung <steve.chung7@gmail.com>
2013-08-12 17:52:09 -0400

    link to companies setup

---
+++ app/helpers/sign_ups_helper.rb
@@ -0,0 +1,15 @@
+module SignUpsHelper
+  def share_url(network, options)
+    if network == :facebook
+    query = {
+        u: options[:url]
+    }.to_query
+    "https://www.facebook.com/sharer/sharer.php?#{query}"
+    else
+      query = {
+          url: options[:url]
+      }.to_query
+      "https://twitter.com/share?#{query}"
+    end
+  end
+end
